78167f6908891d11b55c50cd9c6a0e0f1e19fa50
xiaodongli1986 <xiaodongli@mail.kias.re.kr>
2019-03-22 16:20:53 +0800

    minor changes

---
+++ gitpush.sh
@@ -0,0 +1,11 @@
+git pull
+git add *.sh
+git add *.py
+git add Materials/*
+git add */src/*
+git add */draft/*
+git add */papers/*
+git add */plots/*
+git add README.md
+git commit -m 'minor changes'
+git push
